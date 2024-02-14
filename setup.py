882d0875655657f55ba9ea11e1e25c6f63ec0096
Thomas SANDIER <thomas.sandier@cpe.fr>
2024-02-14 17:41:00 +0100

    ajout du fichier compressé

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
-setup{
+setup(
     name='AdmCO_TP2',
     version='1.0',
     packages=find_packages(exclude=['tests']),
     author="Thomas SANDIER",
     author_email="thomas.sandier@cpe.fr",
     description="Un jeu pierre feuille ciseaux",
-}
+)
