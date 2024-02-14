593a5d108004f00f4e629c65b14312aed12bf3b4
Thomas SANDIER <thomas.sandier@cpe.fr>
2024-02-14 17:38:06 +0100

    ajout du fichier setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup{
+    name='AdmCO_TP2',
+    version='1.0',
+    packages=find_packages(exclude=['tests']),
+    author="Thomas SANDIER",
+    author_email="thomas.sandier@cpe.fr",
+    description="Un jeu pierre feuille ciseaux",
+}
